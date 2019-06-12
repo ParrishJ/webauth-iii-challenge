94e4f300bcfa6553c8e3e064c6d2acbd16005d5b
ParrishJ <jarparrish@gmail.com>
2019-06-12 11:43:01 -0700

    basic structure started

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const helmet = require('helmet');
+const cors = require('cors');
+
+const authorizationRouter = require('../authorization/auth-router.js');
+const userRouter = require('../users/user-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api/users', userRouter);
+server.use('/api', authorizationRouter);
+
+module.exports = server;
+
---
+++ secrets/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || 'SHHH ITS A SECRET!'
+};
